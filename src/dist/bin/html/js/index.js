52eadacda6310c1b2e2fde4698e9bf80c9498217
tangxiaoming <tangxiaoming@MacBook-Pro-2.local>
2018-12-24 18:23:06 +0800

    add jquery

--- src/dist/bin/html/js/index.js
+++ src/dist/bin/html/js/index.js
@@ -12,79 +12,95 @@ void function(){
         xhr.open(method, url, true);
         xhr.send();
     }
+
+    function getJSON(url, callback){
+
+        $.getJSON(apiVersion + url, callback);
+    }
+
+
     function start_staking() {
-        let mnemonic = document.getElementById('start_staking_mnemonic').value;
+        let mnemonic = $('#start_staking_mnemonic').val();
         let url = "/staker/start/" + mnemonic + "/";
 
         request("POST", url, function () {
-            document.getElementById('start_staking_result').value = this.responseText;
+            $('#start_staking_result').val(this.responseText);
         });
     }
+
     function balance() {
-        let account = document.getElementById('balance_account').value;
+        let account = $('#balance_account').val();
         let url = "/ledger/get/" + account + "/";
 
-        request("GET", url, function () {
+        getJSON( url, function (data) {
+            console.log(this.status)
             if (this.status == 404) {
-                document.getElementById('balance_result').value = 0;
+                $('#balance_result').val(0)
                 return;
             }
-            let data = JSON.parse(this.responseText)
-            document.getElementById('balance_result').value = data.balance;
+            let data = JSON.parse(this.responseText);
+            $('#balance_result').val(data.balance);
         });
     }
     function transfer() {
-        let mnemonic = document.getElementById('transfer_mnemonic').value;
-        let fee = document.getElementById('transfer_fee').value;
-        let amount = document.getElementById('transfer_amount').value;
-        let to = document.getElementById('transfer_to').value;
-        let message = document.getElementById('transfer_message').value;
-        let encrypted = message && document.getElementById('transfer_encrypted').checked ? "1" : "";
+        let mnemonic = $('#transfer_mnemonic').val();
+        let fee = $('#transfer_fee').val();
+        let amount = $('#transfer_amount').val();
+        let to = $('#transfer_to').val();
+        let message = $('#transfer_message').val();
+        let encrypted = message && $('#transfer_encrypted').prop('checked') ? "1" : "";
         let url = "/transfer/" + mnemonic + "/" + fee + "/" + amount + "/" + to + "/" + message + "/" + encrypted + "/";
 
         request("POST", url, function () {
-            document.getElementById('transfer_result').value = this.responseText;
+            $('#transfer_result').val(this.responseText);
         });
     }
     function sign() {
-        let mnemonic = document.getElementById('sign_mnemonic').value;
-        let message = document.getElementById('sign_message').value;
+        let mnemonic = $('#sign_mnemonic').val();
+        let message = $('#sign_message').val();
         let url = "/signmessage/" + mnemonic + "/" + message + "/";
 
         request("POST", url, function () {
-            document.getElementById('sign_result').value = this.responseText;
+            $('#sign_result').val(this.responseText);
         });
     }
     function verify() {
-        let account = document.getElementById('verify_account').value;
-        let signature = document.getElementById('verify_signature').value;
-        let message = document.getElementById('verify_message').value;
+        let account = $('#verify_account').val();
+        let signature = $('#verify_signature').val();
+        let message = $('#verify_message').val();
         let url = "/verifymessage/" + account + "/" + signature + "/" + message + "/";
 
         request("GET", url, function () {
-            document.getElementById('verify_result').value = this.responseText;
+            $('#verify_result').val(this.responseText);
         });
     }
     function mnemonic_info() {
-        let mnemonic = document.getElementById('mnemonic_info').value;
+        let mnemonic = $('#mnemonic_info').val();
         let url = "/mnemonic/info/" + mnemonic + "/";
 
         request("POST", url, function () {
-            document.getElementById('mnemonic_info_result').value = this.responseText;
+            $('#mnemonic_info_result').val(this.responseText);
         });
     }
     function request_info() {
-        request("GET", "/ledger", function () {
-            let data = JSON.parse(this.responseText)
-            document.getElementById('info_height').innerHTML = "Blockchain height: " + data.height;
+        getJSON("/ledger", function (data) {
+            $('#info_height').html("Blockchain height: " + data.height);
         });
-        request("GET", "/nodeinfo", function () {
-            let data = JSON.parse(this.responseText)
-            document.getElementById('info_connections').innerHTML = "Connections: " + data.outgoing + " outgoing, " + data.incoming + " incoming";
+        getJSON("/nodeinfo", function (data) {
+            $('#info_connections').html("Connections: " + data.outgoing + " outgoing, " + data.incoming + " incoming");
         });
     }
     request_info();
+
     
-    let ws = new WebSocket("ws://"+location.hostname+"/api/v1/notify/block");
+    $('#start_staking').on('click', start_staking);
+    $('#balance').on('click', balance);
+    $('#transfer').on('click', transfer);
+    $('#sign').on('click', sign);
+    $('#verify').on('click', verify);
+    $('#mnemonic_info').on('click', mnemonic_info);
+
+    
+    let ws = new WebSocket("ws://"+location.host+"/api/v1/notify/block");
     ws.onmessage = request_info;
 }();
