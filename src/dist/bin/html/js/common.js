148f66220095ba5a3ad0599957b4ddf3b3fb894a
blacknet1fcemzzxna <blacknet1fcemzzxna@gmail.com>
2019-03-30 18:17:27 +0800

    add ledger info

---
+++ src/dist/bin/html/js/common.js
@@ -0,0 +1,36 @@
+
+void function(){
+
+    const Blacknet = {};
+    const menu = $('.main-menu'), panel = $('.rightpanel'), apiVersion = "/api/v1", body = $("body");;
+    const hash = localStorage.hashIndex || 'overview';
+    const dialogPassword = $('.dialog.password'), mask = $('.mask');
+    const account = localStorage.account || 'blacknet1mm29uzgw40vl3mtaf3mepserc0vtmuapvmx5l92qxggvx0aqlnysp3v2hz';
+
+
+
+
+    Blacknet.balance = async function(){
+
+        let balance = $('.balance');
+
+        $.getJSON(apiVersion + '/ledger/get/' + account + '/', function(data){
+            balance.html( (data.balance/1e8).toFixed(8) + ' BLN');
+        }).fail(function(){
+            balance.html('0.00000000 BLN');
+        });
+    };
+
+    Blacknet.network = async function(){
+        let network = $('.network');
+        $.getJSON(apiVersion + '/ledger', function(data){
+            
+            network.find('.height').html(data.height);
+            network.find('.supply').html((data.supply/1e8).toFixed(0));
+            network.find('.accounts').html(data.accounts);
+        });
+    };
+
+    Blacknet.network();
+    Blacknet.balance();
+}();
--- src/dist/bin/html/js/new_index.js
+++ src/dist/bin/html/js/new_index.js
@@ -1,16 +1,42 @@
 $(document).ready(function () {
 
-    const menu = $('.main-menu'), panel = $('.rightpanel');
+    const menu = $('.main-menu'), panel = $('.rightpanel'), apiVersion = "/api/v1", body = $("body");;
     const hash = localStorage.hashIndex || 'overview';
+    const dialogPassword = $('.dialog.password'), mask = $('.mask');
 
-
-
-    menu.on('click', 'li', menuSwitch);
+    const account = localStorage.account || 'blacknet1mm29uzgw40vl3mtaf3mepserc0vtmuapvmx5l92qxggvx0aqlnysp3v2hz';
+    
     menu.find('a[data-index="'+hash+'"]').parent().addClass('active');
    
-    panel.find('.'+hash).show();
+    
 
 
+    function start_staking_click() {
+
+        mask.show();
+        dialogPassword.show();
+
+        dialogPassword.find('.confirm').unbind().on('click', function(){
+            start_staking(dialogPassword.find('.mnemonic').val());
+        });
+    }
+
+    function start_staking(mnemonic){
+
+        let url = apiVersion + "/staker/start/" + mnemonic + "/";
+
+        $.post(url, {}, function(){
+            hidePasswordDialog();
+        }).fail(function(){
+            hidePasswordDialog();
+            alert('Invalid mnemonic');
+        });
+    }
+
+    function hidePasswordDialog(){
+         mask.hide();
+         dialogPassword.hide();
+     }
 
         
     function menuSwitch(){
@@ -23,5 +49,20 @@ $(document).ready(function () {
         localStorage.hashIndex = index;
         return false;
     }
-        
+
+    menu.on('click', 'li', menuSwitch);
+    body.on("click", "#start_staking", start_staking_click);
+    panel.find('.'+hash).show();
+        // .on("click", "#stop_staking", stop_staking)
+        // .on("click", "#balance", balance)
+        // .on("click", "#transfer", transfer)
+        // .on("click", "#sign", sign)
+        // .on("click", "#verify", verify)
+        // .on("click", "#mnemonic_info", mnemonic_info)
+        // .on("click", "#generate_new_account", generate_new_account)
+        // .on("click", "#display_api_json_result", function (event) {
+        //     let el = event.target;
+        //     display_api_json_result(el.dataset.type);
+        // });
+    
 });
