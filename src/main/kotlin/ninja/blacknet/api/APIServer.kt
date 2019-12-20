91d5fc5505a62b8ce20a17da91e07316f1deb040
大鼠四 <git@vasin.nl>
2019-12-20 10:44:45 +0300

    还原提交a113cee3724f75454c5f6d0f6562803a4c77b064
    https://github.com/ktorio/ktor/issues/1422

--- src/main/kotlin/ninja/blacknet/api/APIServer.kt
+++ src/main/kotlin/ninja/blacknet/api/APIServer.kt
@@ -29,7 +29,6 @@ import io.ktor.request.receiveParameters
 import io.ktor.response.respond
 import io.ktor.response.respondRedirect
 import io.ktor.response.respondText
-import io.ktor.util.hex
 import io.ktor.routing.get
 import io.ktor.routing.post
 import io.ktor.routing.routing
