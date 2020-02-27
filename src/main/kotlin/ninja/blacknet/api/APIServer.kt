5b992993e71d327720b52cef652eed4abf224119
大鼠四 <git@vasin.nl>
2020-02-27 12:49:30 +0300

    移动钱包超文本传输协议应用程序接口

--- src/main/kotlin/ninja/blacknet/api/APIServer.kt
+++ src/main/kotlin/ninja/blacknet/api/APIServer.kt
@@ -10,7 +10,6 @@
 
 package ninja.blacknet.api
 
-import com.google.common.collect.Maps.newHashMapWithExpectedSize
 import io.ktor.application.Application
 import io.ktor.application.ApplicationCall
 import io.ktor.application.call
@@ -24,12 +23,10 @@ import io.ktor.http.cio.websocket.Frame
 import io.ktor.http.cio.websocket.readText
 import io.ktor.http.content.files
 import io.ktor.http.content.static
-import io.ktor.request.receiveParameters
 import io.ktor.response.respond
 import io.ktor.response.respondRedirect
 import io.ktor.response.respondText
 import io.ktor.routing.get
-import io.ktor.routing.post
 import io.ktor.routing.routing
 import io.ktor.websocket.WebSockets
 import io.ktor.websocket.webSocket
@@ -39,15 +36,10 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 import kotlinx.serialization.SerializationStrategy
-import kotlinx.serialization.internal.HashMapSerializer
-import kotlinx.serialization.json.JsonElement
-import kotlinx.serialization.list
-import kotlinx.serialization.serializer
 import ninja.blacknet.Config
 import ninja.blacknet.Runtime
 import ninja.blacknet.Version
 import ninja.blacknet.api.v1.*
-import ninja.blacknet.coding.toHex
 import ninja.blacknet.core.*
 import ninja.blacknet.crypto.*
 import ninja.blacknet.db.*
@@ -383,136 +375,11 @@ fun Application.APIServer() {
                 call.respond(HttpStatusCode.BadRequest, "Snapshot not found")
         }
 
-        get("/api/v2/generateaccount/{wordlist?}") {
-            val wordlist = Wordlists.get(call.parameters["wordlist"] ?: "english") ?: return@get call.respond(HttpStatusCode.BadRequest, "Invalid wordlist")
-
-            call.respondJson(NewMnemonicInfo.serializer(), NewMnemonicInfo.new(wordlist))
-        }
-
-        get("/api/v2/address/{address}") {
-            val info = AddressInfo.fromString(call.parameters["address"]) ?: return@get call.respond(HttpStatusCode.BadRequest, "Invalid address")
-
-            call.respondJson(AddressInfo.serializer(), info)
-        }
-
-        post("/api/v2/mnemonic") {
-            val parameters = call.receiveParameters()
-            val info = MnemonicInfo.fromString(parameters["mnemonic"]) ?: return@post call.respond(HttpStatusCode.BadRequest, "Invalid mnemonic")
-
-            call.respondJson(MnemonicInfo.serializer(), info)
-        }
-
-        post("/api/v2/decryptmessage") {
-            val parameters = call.receiveParameters()
-            val privateKey = Mnemonic.fromString(parameters["mnemonic"]) ?: return@post call.respond(HttpStatusCode.BadRequest, "Invalid mnemonic")
-            val publicKey = Address.decode(parameters["from"]) ?: return@post call.respond(HttpStatusCode.BadRequest, "Invalid from")
-            val message = parameters["message"] ?: return@post call.respond(HttpStatusCode.BadRequest, "Invalid message")
-
-            val decrypted = Message.decrypt(privateKey, publicKey, message)
-            if (decrypted != null)
-                call.respond(decrypted)
-            else
-                call.respond(HttpStatusCode.BadRequest, "Decryption failed")
-        }
-
-        post("/api/v2/signmessage") {
-            val parameters = call.receiveParameters()
-            val privateKey = Mnemonic.fromString(parameters["mnemonic"]) ?: return@post call.respond(HttpStatusCode.BadRequest, "Invalid mnemonic")
-            val message = parameters["message"] ?: return@post call.respond(HttpStatusCode.BadRequest, "Invalid message")
-
-            val signature = Message.sign(privateKey, message)
-
-            call.respond(signature.toString())
-        }
-
-        get("/api/v2/verifymessage/{from}/{signature}/{message}") {
-            val publicKey = Address.decode(call.parameters["from"]) ?: return@get call.respond(HttpStatusCode.BadRequest, "Invalid from")
-            val signature = Signature.fromString(call.parameters["signature"]) ?: return@get call.respond(HttpStatusCode.BadRequest, "Invalid signature")
-            val message = call.parameters["message"] ?: return@get call.respond(HttpStatusCode.BadRequest, "Invalid message")
-
-            val result = Message.verify(publicKey, signature, message)
-
-            call.respond(result.toString())
-        }
-
-        get("/api/v2/wallet/{address}/transactions") {
-            val publicKey = Address.decode(call.parameters["address"]) ?: return@get call.respond(HttpStatusCode.BadRequest, "Invalid address")
-
-            val transactions = WalletDB.mutex.withLock {
-                val wallet = WalletDB.getWalletImpl(publicKey)
-                val transactions = newHashMapWithExpectedSize<String, JsonElement>(wallet.transactions.size)
-                wallet.transactions.forEach { (hash, txData) ->
-                    transactions.put(hash.toString(), txData.toJson())
-                }
-                transactions
-            }
-            call.respondJson(HashMapSerializer(String.serializer(), JsonElement.serializer()), transactions)
-        }
-
-        get("/api/v2/wallet/{address}/outleases") {
-            val publicKey = Address.decode(call.parameters["address"]) ?: return@get call.respond(HttpStatusCode.BadRequest, "Invalid address")
-
-            WalletDB.mutex.withLock {
-                val wallet = WalletDB.getWalletImpl(publicKey)
-                call.respondJson(AccountState.Lease.serializer().list, wallet.outLeases)
-            }
-        }
-
-        get("/api/v2/wallet/{address}/sequence") {
-            val publicKey = Address.decode(call.parameters["address"]) ?: return@get call.respond(HttpStatusCode.BadRequest, "Invalid address")
-
-            call.respond(WalletDB.getSequence(publicKey).toString())
-        }
-
-        get("/api/v2/wallet/{address}/transaction/{hash}/{raw?}") {
-            val publicKey = Address.decode(call.parameters["address"]) ?: return@get call.respond(HttpStatusCode.BadRequest, "Invalid address")
-            val hash = Hash.fromString(call.parameters["hash"]) ?: return@get call.respond(HttpStatusCode.BadRequest, "Invalid hash")
-            val raw = call.parameters["raw"]?.toBoolean() ?: false
-
-            WalletDB.mutex.withLock {
-                val wallet = WalletDB.getWalletImpl(publicKey)
-                val txData = wallet.transactions.get(hash)
-                if (txData != null) {
-                    val bytes = WalletDB.getTransactionImpl(hash)
-                    if (bytes != null) {
-                        if (raw) {
-                            call.respond(bytes.toHex())
-                        } else {
-                            val tx = Transaction.deserialize(bytes)
-                            call.respondJson(TransactionInfo.serializer(), TransactionInfo(tx, hash, bytes.size, txData.types))
-                        }
-                    } else {
-                        call.respond(HttpStatusCode.BadRequest, "Transaction not found")
-                    }
-                } else {
-                    call.respond(HttpStatusCode.BadRequest, "Transaction not found")
-                }
-            }
-        }
-
-        get("/api/v2/wallet/{address}/confirmations/{hash}") {
-            val publicKey = Address.decode(call.parameters["address"]) ?: return@get call.respond(HttpStatusCode.BadRequest, "Invalid address")
-            val hash = Hash.fromString(call.parameters["hash"]) ?: return@get call.respond(HttpStatusCode.BadRequest, "Invalid hash")
-
-            val result = WalletDB.getConfirmations(publicKey, hash)
-            if (result != null)
-                call.respond(result.toString())
-            else
-                call.respond(HttpStatusCode.BadRequest, "Transaction not found")
-        }
-
-        get("/api/v2/wallet/{address}/referencechain") {
-            @Suppress("UNUSED_VARIABLE")
-            val publicKey = Address.decode(call.parameters["address"]) ?: return@get call.respond(HttpStatusCode.BadRequest, "Invalid address")
-
-            val result = WalletDB.referenceChain()
-            call.respond(result.toString())
-        }
-
         debug()
         sendTransaction()
         staking()
         node()
+        wallet()
 
         // 已被弃用
         APIV1()
