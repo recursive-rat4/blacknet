1fbeb5f620d961b847cbdef63f26d1176533a898
大鼠四 <git@vasin.nl>
2020-04-02 10:55:48 +0300

    面子控制

---
+++ src/main/kotlin/ninja/blacknet/core/Bouncer.kt
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2020 Pavel Vasin
+ *
+ * Licensed under the Jelurida Public License version 1.1
+ * for the Blacknet Public Blockchain Platform (the "License");
+ * you may not use this file except in compliance with the License.
+ * See the LICENSE.txt file at the top-level directory of this distribution.
+ */
+
+package ninja.blacknet.core
+
+/**
+ * 面子控制
+ */
+object Bouncer {
+    private class BouncerException(message: String) : RuntimeException(message)
+}
