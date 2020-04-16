dea8557bf62c27d7062607616e106dbc07c6d199
大鼠四 <git@vasin.nl>
2020-04-16 18:20:59 +0300

    刷新編譯程序

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -8,7 +8,7 @@
  */
 
 object Versions {
-    const val kotlin = "1.3.71"
+    const val kotlin = "1.3.72"
     const val coroutines = "1.3.5"
     const val serialization = "0.20.0"
     const val ktor = "1.3.2"
