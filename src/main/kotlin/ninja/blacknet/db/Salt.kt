07a8327b837ec291c0339e3e9337fc8994f8c490
大鼠四 <git@vasin.nl>
2020-04-24 17:22:22 +0000

    修復內存不足

--- src/main/kotlin/ninja/blacknet/db/Salt.kt
+++ src/main/kotlin/ninja/blacknet/db/Salt.kt
@@ -30,7 +30,7 @@ object Salt {
             saltBytes
         } else {
             if (LevelDB.get(OLD_VERSION_KEY) != null)
-                nibbler()
+                convertor()
 
             val bytes = SecureRandom().nextBytes(16)
 
@@ -42,40 +42,40 @@ object Salt {
         }
     }
 
-    private fun nibbler() {
-        logger.debug { "Nibbling the keys..." }
-        val batch = LevelDB.createWriteBatch()
+    private fun convertor() {
+        logger.info("Convertoring the database...")
         val iterator = LevelDB.iterator()
         iterator.seekToFirst()
         while (iterator.hasNext()) {
             val entry = iterator.next()
             var key: ByteArray?
-            key = Pair("account", PublicKey.SIZE_BYTES) - entry; if (key != null) { nibble(batch, entry, key, DBKey(1, PublicKey.SIZE_BYTES)); continue }
-            key = Pair("chain", Hash.SIZE_BYTES) - entry; if (key != null) { nibble(batch, entry, key, DBKey(2, Hash.SIZE_BYTES)); continue }
-            key = Pair("htlc", Hash.SIZE_BYTES) - entry; if (key != null) { nibble(batch, entry, key, DBKey(3, Hash.SIZE_BYTES)); continue }
-            key = Pair("multisig", Hash.SIZE_BYTES) - entry; if (key != null) { nibble(batch, entry, key, DBKey(4, Hash.SIZE_BYTES)); continue }
-            key = Pair("undo", Hash.SIZE_BYTES) - entry; if (key != null) { nibble(batch, entry, key, DBKey(5, Hash.SIZE_BYTES)); continue }
-            key = Pair("ledgersizes", 0) - entry; if (key != null) { nibble(batch, entry, key, DBKey(6, 0)); continue }
-            key = Pair("ledgersnapshot", Int.SIZE_BYTES) - entry; if (key != null) { nibble(batch, entry, key, DBKey(7, Int.SIZE_BYTES)); continue }
-            key = Pair("ledgersnapshotheights", 0) - entry; if (key != null) { nibble(batch, entry, key, DBKey(8, 0)); continue }
-            key = Pair("ledgerstate", 0) - entry; if (key != null) { nibble(batch, entry, key, DBKey(9, 0)); continue }
-            key = Pair("ledgerversion", 0) - entry; if (key != null) { nibble(batch, entry, key, DBKey(10, 0)); continue }
-            key = Pair("walletkeys", 0) - entry; if (key != null) { nibble(batch, entry, key, DBKey(64, 0)); continue }
-            key = Pair("tx", Hash.SIZE_BYTES) - entry; if (key != null) { nibble(batch, entry, key, DBKey(65, Hash.SIZE_BYTES)); continue }
-            key = Pair("walletversion", 0) - entry; if (key != null) { nibble(batch, entry, key, DBKey(66, 0)); continue }
-            key = Pair("wallet", Hash.SIZE_BYTES) - entry; if (key != null) { nibble(batch, entry, key, DBKey(67, Hash.SIZE_BYTES)); continue }
-            key = Pair("peerdb", 0) - entry; if (key != null) { nibble(batch, entry, key, DBKey(0x80.toByte(), 0)); continue }
-            key = Pair("peerversion", 0) - entry; if (key != null) { nibble(batch, entry, key, DBKey(0x81.toByte(), 0)); continue }
-            key = Pair("block", Hash.SIZE_BYTES) - entry; if (key != null) { nibble(batch, entry, key, DBKey(0xC0.toByte(), Hash.SIZE_BYTES)); continue }
-            logger.info("Unknown key ${entry.key.toHex()}")
+            key = Pair("account", PublicKey.SIZE_BYTES) - entry; if (key != null) { nibble(entry, key, DBKey(1, PublicKey.SIZE_BYTES)); continue }
+            key = Pair("chain", Hash.SIZE_BYTES) - entry; if (key != null) { nibble(entry, key, DBKey(2, Hash.SIZE_BYTES)); continue }
+            key = Pair("htlc", Hash.SIZE_BYTES) - entry; if (key != null) { nibble(entry, key, DBKey(3, Hash.SIZE_BYTES)); continue }
+            key = Pair("multisig", Hash.SIZE_BYTES) - entry; if (key != null) { nibble(entry, key, DBKey(4, Hash.SIZE_BYTES)); continue }
+            key = Pair("undo", Hash.SIZE_BYTES) - entry; if (key != null) { nibble(entry, key, DBKey(5, Hash.SIZE_BYTES)); continue }
+            key = Pair("ledgersizes", 0) - entry; if (key != null) { nibble(entry, key, DBKey(6, 0)); continue }
+            key = Pair("ledgersnapshot", Int.SIZE_BYTES) - entry; if (key != null) { nibble(entry, key, DBKey(7, Int.SIZE_BYTES)); continue }
+            key = Pair("ledgersnapshotheights", 0) - entry; if (key != null) { nibble(entry, key, DBKey(8, 0)); continue }
+            key = Pair("ledgerstate", 0) - entry; if (key != null) { nibble(entry, key, DBKey(9, 0)); continue }
+            key = Pair("ledgerversion", 0) - entry; if (key != null) { nibble(entry, key, DBKey(10, 0)); continue }
+            key = Pair("walletkeys", 0) - entry; if (key != null) { nibble(entry, key, DBKey(64, 0)); continue }
+            key = Pair("tx", Hash.SIZE_BYTES) - entry; if (key != null) { nibble(entry, key, DBKey(65, Hash.SIZE_BYTES)); continue }
+            key = Pair("walletversion", 0) - entry; if (key != null) { nibble(entry, key, DBKey(66, 0)); continue }
+            key = Pair("wallet", Hash.SIZE_BYTES) - entry; if (key != null) { nibble(entry, key, DBKey(67, Hash.SIZE_BYTES)); continue }
+            key = Pair("peerdb", 0) - entry; if (key != null) { nibble(entry, key, DBKey(0x80.toByte(), 0)); continue }
+            key = Pair("peerversion", 0) - entry; if (key != null) { nibble(entry, key, DBKey(0x81.toByte(), 0)); continue }
+            key = Pair("block", Hash.SIZE_BYTES) - entry; if (key != null) { nibble(entry, key, DBKey(0xC0.toByte(), Hash.SIZE_BYTES)); continue }
+            logger.debug { "Unknown key ${entry.key.toHex()}" }
         }
         iterator.close()
-        batch.write()
     }
 
-    private fun nibble(batch: LevelDB.WriteBatch, entry: Map.Entry<ByteArray, ByteArray>, key: ByteArray, dbKey: DBKey) {
+    private fun nibble(entry: Map.Entry<ByteArray, ByteArray>, key: ByteArray, dbKey: DBKey) {
+        val batch = LevelDB.createWriteBatch()
         batch.put(dbKey, key, entry.value)
         batch.delete(entry.key)
+        batch.write()
     }
 }
 
