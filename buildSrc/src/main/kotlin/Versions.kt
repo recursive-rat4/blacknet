9014695ed8329292930923932828b5b8c20f9536
大鼠四 <git@vasin.nl>
2020-05-25 13:32:06 +0000

    更新一個協程子程序庫

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -9,7 +9,7 @@
 
 object Versions {
     const val kotlin = "1.3.72"
-    const val coroutines = "1.3.5"
+    const val coroutines = "1.3.6"
     const val serialization = "0.20.0"
     const val ktor = "1.3.2"
     const val weupnp = "0.1.4"
