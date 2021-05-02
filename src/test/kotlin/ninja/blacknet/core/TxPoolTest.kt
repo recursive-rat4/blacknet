5e1d579c1835ab1ff9509b063384c7c2bd75ae77
Pavel Vasin <rat4@blacknet.ninja>
2021-05-02 09:27:40 +0000

    添加一個測試

---
+++ src/test/kotlin/ninja/blacknet/core/TxPoolTest.kt
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2021 Pavel Vasin
+ *
+ * Licensed under the Jelurida Public License version 1.1
+ * for the Blacknet Public Blockchain Platform (the "License");
+ * you may not use this file except in compliance with the License.
+ * See the LICENSE.txt file at the top-level directory of this distribution.
+ */
+
+package ninja.blacknet.core
+
+import kotlin.test.Test
+
+class TxPoolTest {
+    @Test
+    fun initialization() {
+        TxPool
+    }
+}
