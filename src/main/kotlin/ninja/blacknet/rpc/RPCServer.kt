e9d8eaf3b86238966f09f26cf32d85de5cf26eb8
Pavel Vasin <rat4@blacknet.ninja>
2022-02-14 12:11:35 +0000

    RPCServer: optimize imports

--- src/main/kotlin/ninja/blacknet/rpc/RPCServer.kt
+++ src/main/kotlin/ninja/blacknet/rpc/RPCServer.kt
@@ -31,7 +31,6 @@ import ninja.blacknet.Version
 import ninja.blacknet.core.Block
 import ninja.blacknet.core.ChainIndex
 import ninja.blacknet.core.Transaction
-import ninja.blacknet.crypto.HashSerializer
 import ninja.blacknet.db.WalletDB
 import ninja.blacknet.logging.debug
 import ninja.blacknet.logging.debugMessage
@@ -40,7 +39,6 @@ import ninja.blacknet.rpc.requests.Requests
 import ninja.blacknet.rpc.v1.*
 import ninja.blacknet.rpc.v2.*
 import ninja.blacknet.serialization.json.json
-import ninja.blacknet.util.SynchronizedArrayList
 import ninja.blacknet.util.SynchronizedHashMap
 import ninja.blacknet.util.SynchronizedHashSet
 import ninja.blacknet.util.statusMessage
