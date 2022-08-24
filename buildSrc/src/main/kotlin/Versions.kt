1605b7955cbe1601f4bfa63a80c0dffbc8161670
Pavel Vasin <rat4@blacknet.ninja>
2022-08-24 12:16:54 +0000

    update kotlin

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -8,7 +8,7 @@
  */
 
 object Versions {
-    const val kotlin = "1.4.31"
+    const val kotlin = "1.4.32"
     const val coroutines = "1.3.9"
     const val serialization = "1.1.0"
     const val ktor = "1.4.1"
