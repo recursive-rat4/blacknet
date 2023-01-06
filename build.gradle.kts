ed85a319a552783ef2a9442a2df28274cc6cb911
Pavel Vasin <rat4@blacknet.ninja>
2023-01-06 12:09:15 +0000

    update atomicfu

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ allprojects {
 
 buildscript {
     dependencies {
-        classpath("org.jetbrains.kotlinx:atomicfu-gradle-plugin:0.17.0")
+        classpath("org.jetbrains.kotlinx:atomicfu-gradle-plugin:0.17.1")
     }
 }
 
