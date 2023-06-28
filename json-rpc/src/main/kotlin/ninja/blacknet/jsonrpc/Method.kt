bf9f5972971db7bf1cad7d45a54d78faa1733bc2
Pavel Vasin <git@vasin.nl>
2023-06-28 18:51:41 +0000

    json-rpc: add Method

---
+++ json-rpc/src/main/kotlin/ninja/blacknet/jsonrpc/Method.kt
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2023 Pavel Vasin
+ *
+ * Licensed under the Jelurida Public License version 1.1
+ * for the Blacknet Public Blockchain Platform (the "License");
+ * you may not use this file except in compliance with the License.
+ * See the LICENSE.txt file at the top-level directory of this distribution.
+ */
+
+package ninja.blacknet.jsonrpc
+
+import kotlinx.serialization.Serializable
+
+/**
+ * A type for the `method` field in a request.
+ */
+@Serializable
+internal inline class Method(
+    private val value: String
+) {
+    init {
+        require(!value.startsWith("rpc.")) { "Reserved method $value" }
+    }
+}
---
+++ json-rpc/src/test/kotlin/ninja/blacknet/jsonrpc/MethodTest.kt
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2023 Pavel Vasin
+ *
+ * Licensed under the Jelurida Public License version 1.1
+ * for the Blacknet Public Blockchain Platform (the "License");
+ * you may not use this file except in compliance with the License.
+ * See the LICENSE.txt file at the top-level directory of this distribution.
+ */
+
+package ninja.blacknet.jsonrpc
+
+import kotlinx.serialization.json.Json
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFails
+
+class MethodTest {
+    @Test
+    fun test() {
+        val goodName = "foo"
+        val goodJson = "\"$goodName\""
+        val badName = "rpc.foo"
+        val badJson = "\"$badName\""
+
+        assertEquals(goodJson, Json.encodeToString(Method.serializer(), Method(goodName)))
+        assertEquals(Method(goodName), Json.decodeFromString(Method.serializer(), goodJson))
+        assertFails { Json.encodeToString(Method.serializer(), Method(badName)) }
+        assertFails { Json.decodeFromString(Method.serializer(), badJson) }
+    }
+}
