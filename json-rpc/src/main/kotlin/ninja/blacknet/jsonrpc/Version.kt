fbc1e885aab81a0a2d8ed93fee85d2b0630847c1
Pavel Vasin <git@vasin.nl>
2023-06-28 17:52:28 +0000

    json-rpc: add Version

---
+++ json-rpc/src/main/kotlin/ninja/blacknet/jsonrpc/Version.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2023 Pavel Vasin
+ *
+ * Licensed under the Jelurida Public License version 1.1
+ * for the Blacknet Public Blockchain Platform (the "License");
+ * you may not use this file except in compliance with the License.
+ * See the LICENSE.txt file at the top-level directory of this distribution.
+ */
+
+package ninja.blacknet.jsonrpc
+
+import kotlinx.serialization.Serializable
+
+private const val JSON_RPC_VERSION = "2.0"
+
+/**
+ * A type for the `jsonrpc` field in a request or response.
+ */
+@Serializable
+internal inline class Version private constructor(
+    private val value: String
+) {
+    /**
+     * Use a default version.
+     */
+    constructor() : this(JSON_RPC_VERSION)
+
+    init {
+        require(value == JSON_RPC_VERSION) { "Unknown JSON-RPC version $value" }
+    }
+}
---
+++ json-rpc/src/test/kotlin/ninja/blacknet/jsonrpc/VersionTest.kt
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2023 Pavel Vasin
+ *
+ * Licensed under the Jelurida Public License version 1.1
+ * for the Blacknet Public Blockchain Platform (the "License");
+ * you may not use this file except in compliance with the License.
+ * See the LICENSE.txt file at the top-level directory of this distribution.
+ */
+
+package ninja.blacknet.jsonrpc
+
+import kotlinx.serialization.json.Json
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFails
+
+class VersionTest {
+    @Test
+    fun test() {
+        val good = "\"2.0\""
+        val bad = "\"1.0\""
+
+        assertEquals(good, Json.encodeToString(Version.serializer(), Version()))
+        assertEquals(Version(), Json.decodeFromString(Version.serializer(), good))
+        assertFails { Json.decodeFromString(Version.serializer(), bad) }
+    }
+}
