a50247beffce8a7e9cd88ebb0c1f18b5b2900af8
Pavel Vasin <pavel@vasin.nl>
2023-07-01 10:33:35 +0000

    update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -141,9 +141,9 @@ val test by tasks.existing(Test::class) {
 }
 
 val wrapper by tasks.existing(Wrapper::class) {
-    gradleVersion = "8.1.1"
+    gradleVersion = "8.2"
     distributionType = Wrapper.DistributionType.BIN
-    distributionSha256Sum = "e111cb9948407e26351227dabce49822fb88c37ee72f1d1582a69c68af2e702f"
+    distributionSha256Sum = "38f66cd6eef217b4c35855bb11ea4e9fbc53594ccccb5fb82dfd317ef8c2c5a3"
 }
 
 val defaultSystemProperties: Map<String, Any> = mapOf(
