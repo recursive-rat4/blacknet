88aeedd8598cd98e3ac850d569b0eb22982e0e26
Pavel Vasin <rat4@blackcash.org>
2023-11-15 17:01:22 +0000

    test: add PeerDB.NetworkStat

---
+++ kernel/src/test/kotlin/ninja/blacknet/db/PeerDBTest.kt
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2023 Pavel Vasin
+ *
+ * Licensed under the Jelurida Public License version 1.1
+ * for the Blacknet Public Blockchain Platform (the "License");
+ * you may not use this file except in compliance with the License.
+ * See the LICENSE.txt file at the top-level directory of this distribution.
+ */
+
+package ninja.blacknet.db
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertIs
+import ninja.blacknet.contract.BAppIdSerializer
+import ninja.blacknet.db.PeerDB.NetworkStat
+import ninja.blacknet.db.PeerDB.UptimeStat
+import ninja.blacknet.serialization.bbf.binaryFormat
+import ninja.blacknet.util.byteArrayOfInts
+import ninja.blacknet.util.hashSetOf
+
+class PeerDBTest {
+    @Test
+    fun networkStat() {
+        val stat = NetworkStat(
+            0,
+            "",
+            UptimeStat(0f, 0f, 0f),
+            UptimeStat(0f, 0f, 0f),
+            UptimeStat(0f, 0f, 0f),
+            UptimeStat(0f, 0f, 0f),
+            UptimeStat(0f, 0f, 0f),
+            hashSetOf(
+                ByteArray(BAppIdSerializer.SIZE_BYTES),
+            ),
+        )
+        val bytes = byteArrayOfInts(
+            0, 0, 0, 0, 0, 0, 0, 0,
+            128,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            128 + 1, 0, 0, 0, 0,
+        )
+        assertIs<NetworkStat>(
+            binaryFormat.decodeFromByteArray(NetworkStat.serializer(), bytes)
+        )
+        assertEquals(
+            bytes,
+            binaryFormat.encodeToByteArray(NetworkStat.serializer(), stat)
+        )
+    }
+}
