a976bbe0686beb11e521223d5f7c0f0da90a090f
Pavel Vasin <rat4@blackcash.org>
2024-03-28 19:40:10 +0000

    io: delimit input stream

---
+++ io/src/test/kotlin/ninja/blacknet/io/DelimitedInputStreamTest.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2023-2024 Pavel Vasin
+ *
+ * Licensed under the Jelurida Public License version 1.1
+ * for the Blacknet Public Blockchain Platform (the "License");
+ * you may not use this file except in compliance with the License.
+ * See the LICENSE.txt file at the top-level directory of this distribution.
+ */
+
+package ninja.blacknet.io
+
+import kotlin.test.Test
+import kotlin.test.assertFails
+
+class DelimitedInputStreamTest {
+    @Test
+    fun valid() {
+        ZeroInputStream().delimited().apply {
+            read(ByteArray(0))
+            read()
+            begin(1)
+            read(ByteArray(0))
+            read()
+            end()
+            read(ByteArray(0))
+            read()
+        }.close()
+    }
+
+    @Test
+    fun trailing() {
+        ZeroInputStream().delimited().apply {
+            begin(2)
+            read(ByteArray(0))
+            read()
+            assertFails { end() }
+        }.close()
+    }
+
+    @Test
+    fun eom() {
+        ZeroInputStream().delimited().apply {
+            begin(1)
+            read()
+            assertFails { read() }
+        }.close()
+    }
+}
