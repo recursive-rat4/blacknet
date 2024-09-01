86ab768b77e77d0b5963f3a9c3d3bac69f7855e5
Pavel Vasin <rat4@blacknet.ninja>
2024-09-01 17:33:22 +0000

    dependencies: update kotlin

--- desktop/src/main/kotlin/ninja/blacknet/swing/dsl/BorderLayout.kt
+++ desktop/src/main/kotlin/ninja/blacknet/swing/dsl/BorderLayout.kt
@@ -1,11 +1,13 @@
 /*
- * Copyright (c) 2023 Pavel Vasin
+ * Copyright (c) 2023-2024 Pavel Vasin
  *
  * Licensed under the Jelurida Public License version 1.1
  * for the Blacknet Public Blockchain Platform (the "License");
  * you may not use this file except in compliance with the License.
  * See the LICENSE.txt file at the top-level directory of this distribution.
  */
+
+@file:Suppress("CONTEXT_RECEIVERS_DEPRECATED")
 
 package ninja.blacknet.swing.dsl
 
