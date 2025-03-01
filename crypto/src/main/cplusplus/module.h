af54e914fc2168577787cb36be314d7322bbf944
Pavel Vasin <rat4@blacknet.ninja>
2025-03-01 10:36:44 +0000

    crypto: implement module

---
+++ crypto/src/main/cplusplus/module.h
@@ -0,0 +1,124 @@
+/*
+ * Copyright (c) 2025 Pavel Vasin
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef BLACKNET_CRYPTO_MODULE_H
+#define BLACKNET_CRYPTO_MODULE_H
+
+#include <algorithm>
+#include <array>
+#include <iostream>
+
+#include "util.h"
+
+template<
+    typename R,
+    std::size_t N
+>
+struct Module {
+    consteval static Module identity() {
+        Module t;
+        t.components.fill(R(0));
+        return t;
+    }
+
+    std::array<R, N> components;
+
+    consteval Module() : components() {}
+    constexpr Module(const Module& other) : components(other.components) {}
+    constexpr Module(const std::array<R, N>& components) : components(components) {}
+
+    constexpr bool operator == (const Module&) const = default;
+
+    consteval static std::size_t rank() {
+        return N;
+    }
+
+    constexpr Module& operator += (const Module& other) {
+        for (std::size_t i = 0; i < N; ++i)
+            components[i] += other.components[i];
+        return *this;
+    }
+
+    constexpr Module operator + (const Module& other) const {
+        Module r;
+        for (std::size_t i = 0; i < N; ++i)
+            r.components[i] = components[i] + other.components[i];
+        return r;
+    }
+
+    constexpr Module operator * (const R& other) const {
+        Module r;
+        for (std::size_t i = 0; i < N; ++i)
+            r.components[i] = components[i] * other;
+        return r;
+    }
+
+    friend constexpr Module operator * (const R& lps, const Module& rps) {
+        Module r;
+        for (std::size_t i = 0; i < N; ++i)
+            r.components[i] = lps * rps.components[i];
+        return r;
+    }
+
+    constexpr Module& operator -= (const Module& other) {
+        for (std::size_t i = 0; i < N; ++i)
+            components[i] -= other.components[i];
+        return *this;
+    }
+
+    constexpr Module operator - (const Module& other) const {
+        Module r;
+        for (std::size_t i = 0; i < N; ++i)
+            r.components[i] = components[i] - other.components[i];
+        return r;
+    }
+
+    constexpr Module operator - () const {
+        Module r;
+        for (std::size_t i = 0; i < N; ++i)
+            r.components[i] = -components[i];
+        return r;
+    }
+
+    friend std::ostream& operator << (std::ostream& out, const Module& val)
+    {
+        return out << val.components;
+    }
+
+    template<typename DRG>
+    constexpr static Module squeeze(DRG& drg) {
+        Module t;
+        std::ranges::generate(t.components, [&] { return R::squeeze(drg); });
+        return t;
+    }
+
+    template<typename RNG>
+    static Module random(RNG& rng) {
+        Module t;
+        std::ranges::generate(t.components, [&] { return R::random(rng); });
+        return t;
+    }
+
+    template<typename RNG, typename DST>
+    static Module random(RNG& rng, const DST& dst) {
+        Module t;
+        std::ranges::generate(t.components, [&] { return R::random(rng, dst); });
+        return t;
+    }
+};
+
+#endif
---
+++ crypto/src/test/cplusplus/module.cpp
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2025 Pavel Vasin
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <boost/test/unit_test.hpp>
+
+#include "module.h"
+#include "pervushin.h"
+
+BOOST_AUTO_TEST_SUITE(Modules)
+
+using R = PervushinRing;
+using M = Module<R, 2>;
+
+BOOST_AUTO_TEST_CASE(test) {
+    R r(3);
+    R s(5);
+    M x({R(7), R(11)});
+    M y({R(13), R(17)});
+
+    BOOST_TEST(r * (x + y) == r * x + r * y);
+    BOOST_TEST((r + s) * x == r * x + s * x);
+    BOOST_TEST((r * s) * x == r * (s * x));
+    BOOST_TEST(R(1) * x == x);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
