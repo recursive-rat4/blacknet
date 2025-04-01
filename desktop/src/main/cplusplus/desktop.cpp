74f452be5930db7187139d6bd7e8974d639e8350
rat4 <rat4@blacknet.ninja>
2025-04-01 16:18:15 +0000

    dependencies: add qt
    https://gitlab.com/blacknet-ninja/blacknet/-/issues/87

---
+++ desktop/src/main/cplusplus/desktop.cpp
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2025 Pavel Vasin
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "blacknet-config.h"
+
+#include <QApplication>
+#include <QIcon>
+
+class Desktop : public QApplication {
+public:
+    Desktop(int& argc, char* argv[]) : QApplication(argc, argv) {
+        setApplicationName("Blacknet Desktop");
+        setApplicationVersion(BLACKNET_VERSION_STRING);
+        setOrganizationDomain("blacknet.ninja");
+        setOrganizationName("Blacknet");
+        setApplicationDisplayName("Blacknet");
+        setDesktopFileName("blacknet-desktop");
+        setQuitOnLastWindowClosed(false);
+        setWindowIcon(QIcon(":/blacknet/resources/logo.png"));
+    }
+};
+
+int main(int argc, char* argv[]) {
+    Desktop desktop(argc, argv);
+    return desktop.exec();
+}
