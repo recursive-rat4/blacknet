453890a115141d5c1180113add4def74fa5bd834
rat4 <rat4@blacknet.ninja>
2025-07-29 16:26:24 +0000

    crypto: linear combination

--- crypto/src/main/rust/circuitbuilder.rs
+++ crypto/src/main/rust/circuitbuilder.rs
@@ -190,14 +190,249 @@ impl<R: Ring> Mul<Variable<R>> for Constant<R> {
     }
 }
 
+type Term<R> = (Variable<R>, Constant<R>);
+
 pub struct LinearCombination<R: Ring> {
     terms: BTreeMap<Variable<R>, Constant<R>>,
 }
 
-impl<R: Ring, const N: usize> From<[(Variable<R>, Constant<R>); N]> for LinearCombination<R> {
-    fn from(terms: [(Variable<R>, Constant<R>); N]) -> Self {
+impl<R: Ring> From<Term<R>> for LinearCombination<R> {
+    fn from(term: Term<R>) -> Self {
         Self {
-            terms: BTreeMap::from(terms),
+            terms: [term].into(),
+        }
+    }
+}
+
+impl<R: Ring, const N: usize> From<[Term<R>; N]> for LinearCombination<R> {
+    fn from(terms: [Term<R>; N]) -> Self {
+        Self {
+            terms: terms.into(),
+        }
+    }
+}
+
+impl<R: Ring> AddAssign<Term<R>> for LinearCombination<R> {
+    fn add_assign(&mut self, rps: Term<R>) {
+        let (variable, coefficient) = rps;
+        self.terms
+            .entry(variable)
+            .and_modify(|value| *value += coefficient)
+            .or_insert(coefficient);
+    }
+}
+
+impl<R: Ring> Add<Constant<R>> for LinearCombination<R> {
+    type Output = Self;
+
+    fn add(mut self, rps: Constant<R>) -> Self::Output {
+        self += rps;
+        self
+    }
+}
+
+impl<R: Ring> AddAssign<Constant<R>> for LinearCombination<R> {
+    fn add_assign(&mut self, rps: Constant<R>) {
+        *self += (Variable::CONSTANT, rps)
+    }
+}
+
+impl<R: Ring> Add<LinearCombination<R>> for Constant<R> {
+    type Output = LinearCombination<R>;
+
+    fn add(self, mut rps: LinearCombination<R>) -> Self::Output {
+        rps += (Variable::CONSTANT, self);
+        rps
+    }
+}
+
+impl<R: Ring> Add<Variable<R>> for LinearCombination<R> {
+    type Output = Self;
+
+    fn add(mut self, rps: Variable<R>) -> Self::Output {
+        self += rps;
+        self
+    }
+}
+
+impl<R: Ring> AddAssign<Variable<R>> for LinearCombination<R> {
+    fn add_assign(&mut self, rps: Variable<R>) {
+        *self += (rps, Constant::UNITY)
+    }
+}
+
+impl<R: Ring> Add<LinearCombination<R>> for Variable<R> {
+    type Output = LinearCombination<R>;
+
+    fn add(self, mut rps: LinearCombination<R>) -> Self::Output {
+        rps += (self, Constant::UNITY);
+        rps
+    }
+}
+
+impl<R: Ring> Add for LinearCombination<R> {
+    type Output = Self;
+
+    fn add(mut self, rps: Self) -> Self::Output {
+        self += rps;
+        self
+    }
+}
+
+impl<R: Ring> AddAssign for LinearCombination<R> {
+    fn add_assign(&mut self, rps: Self) {
+        for term in rps.terms {
+            *self += term
+        }
+    }
+}
+
+impl<R: Ring> Neg for LinearCombination<R> {
+    type Output = Self;
+
+    fn neg(mut self) -> Self::Output {
+        for coefficient in self.terms.values_mut() {
+            *coefficient = -*coefficient;
+        }
+        self
+    }
+}
+
+impl<R: Ring> SubAssign<Term<R>> for LinearCombination<R> {
+    fn sub_assign(&mut self, rps: Term<R>) {
+        let (variable, coefficient) = rps;
+        self.terms
+            .entry(variable)
+            .and_modify(|value| *value -= coefficient)
+            .or_insert(coefficient);
+    }
+}
+
+impl<R: Ring> Sub<Constant<R>> for LinearCombination<R> {
+    type Output = Self;
+
+    fn sub(mut self, rps: Constant<R>) -> Self::Output {
+        self -= rps;
+        self
+    }
+}
+
+impl<R: Ring> SubAssign<Constant<R>> for LinearCombination<R> {
+    fn sub_assign(&mut self, rps: Constant<R>) {
+        *self -= (Variable::CONSTANT, rps)
+    }
+}
+
+impl<R: Ring> Sub<LinearCombination<R>> for Constant<R> {
+    type Output = LinearCombination<R>;
+
+    fn sub(self, mut rps: LinearCombination<R>) -> Self::Output {
+        rps -= (Variable::CONSTANT, self);
+        rps
+    }
+}
+
+impl<R: Ring> Sub<Variable<R>> for LinearCombination<R> {
+    type Output = Self;
+
+    fn sub(mut self, rps: Variable<R>) -> Self::Output {
+        self -= rps;
+        self
+    }
+}
+
+impl<R: Ring> SubAssign<Variable<R>> for LinearCombination<R> {
+    fn sub_assign(&mut self, rps: Variable<R>) {
+        *self -= (rps, Constant::UNITY)
+    }
+}
+
+impl<R: Ring> Sub<LinearCombination<R>> for Variable<R> {
+    type Output = LinearCombination<R>;
+
+    fn sub(self, mut rps: LinearCombination<R>) -> Self::Output {
+        rps -= (self, Constant::UNITY);
+        rps
+    }
+}
+
+impl<R: Ring> Sub for LinearCombination<R> {
+    type Output = Self;
+
+    fn sub(mut self, rps: Self) -> Self::Output {
+        self -= rps;
+        self
+    }
+}
+
+impl<R: Ring> SubAssign for LinearCombination<R> {
+    fn sub_assign(&mut self, rps: Self) {
+        for term in rps.terms {
+            *self -= term
+        }
+    }
+}
+
+impl<R: Ring> Mul<Constant<R>> for LinearCombination<R> {
+    type Output = Self;
+
+    fn mul(mut self, rps: Constant<R>) -> Self::Output {
+        self *= rps;
+        self
+    }
+}
+
+impl<R: Ring> MulAssign<Constant<R>> for LinearCombination<R> {
+    fn mul_assign(&mut self, rps: Constant<R>) {
+        for coefficient in self.terms.values_mut() {
+            *coefficient *= rps;
+        }
+    }
+}
+
+impl<R: Ring> Mul<LinearCombination<R>> for Constant<R> {
+    type Output = LinearCombination<R>;
+
+    fn mul(self, mut rps: LinearCombination<R>) -> Self::Output {
+        for coefficient in rps.terms.values_mut() {
+            *coefficient = self * *coefficient;
+        }
+        rps
+    }
+}
+
+impl<R: Ring> Mul<Variable<R>> for LinearCombination<R> {
+    type Output = LinearMonoid<R>;
+
+    fn mul(self, rps: Variable<R>) -> Self::Output {
+        [self, (rps, Constant::UNITY).into()].into()
+    }
+}
+
+impl<R: Ring> Mul<LinearCombination<R>> for Variable<R> {
+    type Output = LinearMonoid<R>;
+
+    fn mul(self, rps: LinearCombination<R>) -> Self::Output {
+        [(self, Constant::UNITY).into(), rps].into()
+    }
+}
+
+impl<R: Ring> Mul for LinearCombination<R> {
+    type Output = LinearMonoid<R>;
+
+    fn mul(self, rps: Self) -> Self::Output {
+        [self, rps].into()
+    }
+}
+
+pub struct LinearMonoid<R: Ring> {
+    factors: Vec<LinearCombination<R>>,
+}
+
+impl<R: Ring, const N: usize> From<[LinearCombination<R>; N]> for LinearMonoid<R> {
+    fn from(factors: [LinearCombination<R>; N]) -> Self {
+        Self {
+            factors: factors.into(),
         }
     }
 }
