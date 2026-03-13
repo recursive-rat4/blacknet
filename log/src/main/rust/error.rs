b42ea06e7b414b6016f3ebebf8f9525a3763c3d6
rat4 <rat4@blacknet.ninja>
2026-03-13 15:40:34 +0000

    log: remove thiserror

--- log/src/main/rust/error.rs
+++ log/src/main/rust/error.rs
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 Pavel Vasin
+ * Copyright (c) 2025-2026 Pavel Vasin
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -15,59 +15,49 @@
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
-use spdlog::error::Error as Up;
+use core::fmt;
+use spdlog::error::Error as SpdlogError;
 use spdlog::error::InvalidArgumentError;
-use thiserror::Error;
+use std::io::Error as IoError;
 
-#[derive(Debug, Error)]
+#[derive(Debug)]
 pub enum Error {
-    #[error("Not unicode data in environment variable BLACKNET_LOGLEVEL")]
     NotUnicodeLogLevel,
 
-    #[error("{0}")]
-    FormatRecord(core::fmt::Error),
+    FormatRecord(fmt::Error),
 
-    #[error("{0}")]
-    WriteRecord(std::io::Error),
-    #[error("{0}")]
-    FlushBuffer(std::io::Error),
-    #[error("{0}")]
-    CreateDirectory(std::io::Error),
-    #[error("{0}")]
-    OpenFile(std::io::Error),
-    #[error("{0}")]
-    QueryFileMetadata(std::io::Error),
-    #[error("{0}")]
-    RenameFile(std::io::Error),
-    #[error("{0}")]
-    RemoveFile(std::io::Error),
-    #[error("{0}")]
+    WriteRecord(IoError),
+    FlushBuffer(IoError),
+    CreateDirectory(IoError),
+    OpenFile(IoError),
+    QueryFileMetadata(IoError),
+    RenameFile(IoError),
+    RemoveFile(IoError),
     ParseLevel(String),
 
-    #[error("{0}")]
     InvalidArgument(InvalidArgumentError),
 
-    #[error("{0:?}")]
     Multiple(Vec<Error>),
-    #[error("{0}")]
     Upstream(String),
 }
 
-impl From<Up> for Error {
-    fn from(err: Up) -> Self {
+impl From<SpdlogError> for Error {
+    fn from(err: SpdlogError) -> Self {
         match err {
-            Up::FormatRecord(err) => Self::FormatRecord(err),
-            Up::WriteRecord(err) => Self::WriteRecord(err),
-            Up::FlushBuffer(err) => Self::FlushBuffer(err),
-            Up::CreateDirectory(err) => Self::CreateDirectory(err),
-            Up::OpenFile(err) => Self::OpenFile(err),
-            Up::QueryFileMetadata(err) => Self::QueryFileMetadata(err),
-            Up::RenameFile(err) => Self::RenameFile(err),
-            Up::RemoveFile(err) => Self::RemoveFile(err),
-            Up::ParseLevel(err) => Self::ParseLevel(err),
-            Up::InvalidArgument(err) => Self::InvalidArgument(err),
-            Up::Multiple(err) => Self::Multiple(err.into_iter().map(Error::from).collect()),
-            Up::Downstream(err) => Self::Upstream(err.to_string()),
+            SpdlogError::FormatRecord(err) => Self::FormatRecord(err),
+            SpdlogError::WriteRecord(err) => Self::WriteRecord(err),
+            SpdlogError::FlushBuffer(err) => Self::FlushBuffer(err),
+            SpdlogError::CreateDirectory(err) => Self::CreateDirectory(err),
+            SpdlogError::OpenFile(err) => Self::OpenFile(err),
+            SpdlogError::QueryFileMetadata(err) => Self::QueryFileMetadata(err),
+            SpdlogError::RenameFile(err) => Self::RenameFile(err),
+            SpdlogError::RemoveFile(err) => Self::RemoveFile(err),
+            SpdlogError::ParseLevel(err) => Self::ParseLevel(err),
+            SpdlogError::InvalidArgument(err) => Self::InvalidArgument(err),
+            SpdlogError::Multiple(err) => {
+                Self::Multiple(err.into_iter().map(Error::from).collect())
+            }
+            SpdlogError::Downstream(err) => Self::Upstream(err.to_string()),
             _ => Self::Upstream(err.to_string()),
         }
     }
@@ -79,5 +69,32 @@ impl From<log::SetLoggerError> for Error {
         Self::Upstream(err.to_string())
     }
 }
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::NotUnicodeLogLevel => {
+                f.write_str("Not unicode data in environment variable BLACKNET_LOGLEVEL")
+            }
+            Self::FormatRecord(err) => write!(f, "{err}"),
+
+            Self::WriteRecord(err) => write!(f, "{err}"),
+            Self::FlushBuffer(err) => write!(f, "{err}"),
+            Self::CreateDirectory(err) => write!(f, "{err}"),
+            Self::OpenFile(err) => write!(f, "{err}"),
+            Self::QueryFileMetadata(err) => write!(f, "{err}"),
+            Self::RenameFile(err) => write!(f, "{err}"),
+            Self::RemoveFile(err) => write!(f, "{err}"),
+            Self::ParseLevel(msg) => f.write_str(msg),
+
+            Self::InvalidArgument(err) => write!(f, "{err}"),
+
+            Self::Multiple(errs) => write!(f, "{errs:?}"),
+            Self::Upstream(msg) => f.write_str(msg),
+        }
+    }
+}
+
+impl core::error::Error for Error {}
 
 pub type Result<T> = core::result::Result<T, Error>;
