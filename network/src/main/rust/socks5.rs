79c5478888f11103e44875a5f686d2bfba4a38d7
rat4 <rat4@blacknet.ninja>
2026-08-07 18:16:01 +0000

    network: split socks stream

--- network/src/main/rust/socks5.rs
+++ network/src/main/rust/socks5.rs
@@ -18,8 +18,9 @@
 use crate::endpoint::Endpoint;
 use core::fmt;
 use std::io::Error as IoError;
-use tokio::io::{AsyncReadExt, AsyncWriteExt, BufStream};
+use tokio::io::{AsyncReadExt, AsyncWriteExt, BufReader, BufWriter};
 use tokio::net::TcpStream;
+use tokio::net::tcp::{OwnedReadHalf, OwnedWriteHalf};
 
 const VERSION: u8 = 5;
 const NO_AUTHENTICATION: u8 = 0;
@@ -29,46 +30,50 @@ const IPV4_ADDRESS: u8 = 1;
 const DOMAIN_NAME: u8 = 3;
 const IPV6_ADDRESS: u8 = 4;
 
-pub async fn socks5(proxy: Endpoint, destination: Endpoint) -> Result<BufStream<TcpStream>, Error> {
+pub async fn socks5(
+    proxy: Endpoint,
+    destination: Endpoint,
+) -> Result<(BufReader<OwnedReadHalf>, BufWriter<OwnedWriteHalf>), Error> {
     let endpoint = proxy.to_rust().ok_or(Error::Endpoint)?;
     let socket = TcpStream::connect(endpoint).await?;
-    let mut stream = BufStream::new(socket);
+    let (tcp_read, tcp_write) = socket.into_split();
+    let (mut buf_reader, mut buf_writer) = (BufReader::new(tcp_read), BufWriter::new(tcp_write));
 
-    stream.write_u8(VERSION).await?;
-    stream.write_u8(1).await?; // number of authentication methods supported
-    stream.write_u8(NO_AUTHENTICATION).await?;
-    stream.flush().await?;
+    buf_writer.write_u8(VERSION).await?;
+    buf_writer.write_u8(1).await?; // number of authentication methods supported
+    buf_writer.write_u8(NO_AUTHENTICATION).await?;
+    buf_writer.flush().await?;
 
-    let version = stream.read_u8().await?;
+    let version = buf_reader.read_u8().await?;
     if version != VERSION {
         return Err(Error::Version(version));
     }
-    let authentication = stream.read_u8().await?;
+    let authentication = buf_reader.read_u8().await?;
     if authentication != NO_AUTHENTICATION {
         return Err(Error::Authentication(authentication));
     }
 
-    stream.write_u8(VERSION).await?;
-    stream.write_u8(TCP_CONNECTION).await?;
-    stream.write_u8(0).await?; // reserved
+    buf_writer.write_u8(VERSION).await?;
+    buf_writer.write_u8(TCP_CONNECTION).await?;
+    buf_writer.write_u8(0).await?; // reserved
     let port = match destination {
         Endpoint::IPv4 { port, address } => {
-            stream.write_u8(IPV4_ADDRESS).await?;
-            stream.write_all(&address).await?;
+            buf_writer.write_u8(IPV4_ADDRESS).await?;
+            buf_writer.write_all(&address).await?;
             port
         }
         Endpoint::IPv6 { port, address } => {
-            stream.write_u8(IPV6_ADDRESS).await?;
-            stream.write_all(&address).await?;
+            buf_writer.write_u8(IPV6_ADDRESS).await?;
+            buf_writer.write_all(&address).await?;
             port
         }
         Endpoint::TORv3 { port, address: _ } => {
             let domainname = destination.to_host();
             let bytes = domainname.as_bytes();
             if bytes.len() <= u8::MAX.into() {
-                stream.write_u8(DOMAIN_NAME).await?;
-                stream.write_u8(bytes.len() as u8).await?;
-                stream.write_all(bytes).await?;
+                buf_writer.write_u8(DOMAIN_NAME).await?;
+                buf_writer.write_u8(bytes.len() as u8).await?;
+                buf_writer.write_all(bytes).await?;
                 port
             } else {
                 return Err(Error::Destination);
@@ -76,35 +81,35 @@ pub async fn socks5(proxy: Endpoint, destination: Endpoint) -> Result<BufStream<
         }
         _ => return Err(Error::Destination),
     };
-    stream.write_u16(port).await?;
-    stream.flush().await?;
+    buf_writer.write_u16(port).await?;
+    buf_writer.flush().await?;
 
-    let version = stream.read_u8().await?;
+    let version = buf_reader.read_u8().await?;
     if version != VERSION {
         return Err(Error::Version(version));
     }
-    let reply = stream.read_u8().await?;
+    let reply = buf_reader.read_u8().await?;
     if reply != REQUEST_GRANTED {
         return Err(Error::NotGranted(reply));
     }
-    let reserved = stream.read_u8().await?;
+    let reserved = buf_reader.read_u8().await?;
     if reserved != 0 {
         return Err(Error::Reserved(reserved));
     }
-    let address_type = stream.read_u8().await?;
+    let address_type = buf_reader.read_u8().await?;
     match address_type {
-        IPV4_ADDRESS => stream.read_exact(&mut [0u8; 4 + 2]).await?,
-        IPV6_ADDRESS => stream.read_exact(&mut [0u8; 16 + 2]).await?,
+        IPV4_ADDRESS => buf_reader.read_exact(&mut [0u8; 4 + 2]).await?,
+        IPV6_ADDRESS => buf_reader.read_exact(&mut [0u8; 16 + 2]).await?,
         DOMAIN_NAME => {
             let mut buf = [0u8; u8::MAX as usize];
-            let len = stream.read_u8().await?;
+            let len = buf_reader.read_u8().await?;
             let slice = &mut buf[..len as usize + 2];
-            stream.read_exact(slice).await?
+            buf_reader.read_exact(slice).await?
         }
         _ => return Err(Error::Unknown(address_type)),
     };
 
-    Ok(stream)
+    Ok((buf_reader, buf_writer))
 }
 
 #[derive(Debug)]
