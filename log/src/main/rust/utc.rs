5a9e38197fe5d293d76a38d08cd23adf61008387
rat4 <rat4@blacknet.ninja>
2026-09-04 19:03:17 +0000

    dependencies: remove chrono

--- log/src/main/rust/utc.rs
+++ log/src/main/rust/utc.rs
@@ -15,7 +15,6 @@
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
-use chrono::{DateTime, Datelike, Timelike, Utc};
 use core::fmt::{Error as FmtError, Write};
 use spdlog::{
     error::Error as LogError,
@@ -33,18 +32,11 @@ impl UTC {
     }
 
     pub fn format<W: Write>(&self, st: SystemTime, write: &mut W) -> Result<(), FmtError> {
-        let dt: DateTime<Utc> = st.into();
-        let millisecond = dt.nanosecond() / 1000000;
+        let dt = DateTime::new(st);
         write!(
             write,
             "{}-{:02}-{:02} {:02}:{:02}:{:02}.{:03}",
-            dt.year(),
-            dt.month(),
-            dt.day(),
-            dt.hour(),
-            dt.minute(),
-            dt.second(),
-            millisecond,
+            dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second, dt.millisecond,
         )
     }
 }
@@ -59,4 +51,58 @@ impl Pattern for UTC {
         self.format(record.time(), dest)
             .map_err(LogError::FormatRecord)
     }
+}
+
+struct DateTime {
+    year: i32,
+    month: u8,
+    day: u8,
+    hour: u8,
+    minute: u8,
+    second: u8,
+    millisecond: u16,
+}
+
+impl DateTime {
+    fn new(st: SystemTime) -> Self {
+        let (seconds, nanosecond) = match st.duration_since(SystemTime::UNIX_EPOCH) {
+            Ok(since) => (since.as_secs().cast_signed(), since.subsec_nanos()),
+            Err(until) => {
+                let until = until.duration();
+                let nanosecond = until.subsec_nanos();
+                if nanosecond != 0 {
+                    (-until.as_secs().cast_signed() - 1, 1000000000 - nanosecond)
+                } else {
+                    (-until.as_secs().cast_signed(), nanosecond)
+                }
+            }
+        };
+        let millisecond = (nanosecond / 1000000) as u16;
+        let (minutes, second) = (seconds.div_euclid(60), seconds.rem_euclid(60) as u8);
+        let (hours, minute) = (minutes.div_euclid(60), minutes.rem_euclid(60) as u8);
+        let (days, hour) = (hours.div_euclid(24), hours.rem_euclid(24) as u8);
+        let (year, month, day) = ymd_from_days(days);
+        Self {
+            year,
+            month,
+            day,
+            hour,
+            minute,
+            second,
+            millisecond,
+        }
+    }
+}
+
+const fn ymd_from_days(mut days: i64) -> (i32, u8, u8) {
+    days += 719468;
+    let (era, doe) = (days.div_euclid(146097), days.rem_euclid(146097));
+    let yoe = (doe - doe / 1460 + doe / 36524 - doe / 146096) / 365;
+    let mut y = era * 400 + yoe;
+    let doy = doe - (yoe * 365 + yoe / 4 - yoe / 100);
+    let mut m = (doy * 5 + 2) / 153;
+    let d = doy - (m * 153 + 2) / 5 + 1;
+    m += if m < 10 { 3 } else { -9 };
+    y += (m < 3) as i64;
+    (y as i32, m as u8, d as u8)
 }
--- log/src/test/rust/utc.rs
+++ log/src/test/rust/utc.rs
@@ -22,10 +22,21 @@ use std::time::SystemTime;
 #[test]
 fn format() {
     let utc = UTC::new();
-    let a = "2097-12-31 23:59:59.999";
-    let mut b = String::with_capacity(a.len());
-    let d = Duration::from_millis(4039372799999);
-    let st = SystemTime::UNIX_EPOCH + d;
-    utc.format(st, &mut b).unwrap();
-    assert_eq!(b, a);
+    for (t, s) in [
+        (i64::MIN, "-292275055-05-16 16:47:04.192"),
+        (-68729844735001, "-208-01-14 16:27:44.999"),
+        (0, "1970-01-01 00:00:00.000"),
+        (1545555600000, "2018-12-23 09:00:00.000"),
+        (i64::MAX, "292278994-08-17 07:12:55.807"),
+    ] {
+        let mut b = String::with_capacity(s.len());
+        let d = Duration::from_millis(t.unsigned_abs());
+        let st = if t > 0 {
+            SystemTime::UNIX_EPOCH + d
+        } else {
+            SystemTime::UNIX_EPOCH - d
+        };
+        utc.format(st, &mut b).unwrap();
+        assert_eq!(b, s);
+    }
 }
